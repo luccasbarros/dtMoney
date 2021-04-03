928408dda30f4825055d1429695ba9883d0e588e
Luccas Barros <luccas.candido@br.clara.net>
2021-04-03 08:06:57 -0300

    style: configurações css global

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
+import {GlobalStyle} from './styles/global'
 
 export function App() {
   return (
     <div>
      <h1>Hello World</h1>
+     <GlobalStyle />
     </div>
   );
 }
-
-
