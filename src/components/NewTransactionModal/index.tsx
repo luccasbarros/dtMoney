b9d9391e71b9b89cb47414538b5ee6739ea9342f
Luccas Barros <luccas.candido@br.clara.net>
2021-04-06 07:29:49 -0300

    style: color selector by click

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
 import Modal from "react-modal";
-import { Container, TransactionTypeContainer } from "./styles";
-import closeImg from '../../assets/close.svg';
-import incomeImg from '../../assets/income.svg';
-import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
+import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import { useState } from "react";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -13,16 +14,21 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
-  return (
-    <Modal 
-    onRequestClose={onRequestClose} 
-    isOpen={isOpen}
-    overlayClassName="react-modal-overlay"
-    className="react-modal-content"
-    >
+  const [type, setType] = useState("deposit");
 
-      <button type="button" onClick={onRequestClose} className="react-modal-close">
-        <img src={closeImg} alt="Fechar modal"/>
+  return (
+    <Modal
+      onRequestClose={onRequestClose}
+      isOpen={isOpen}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar modal" />
       </button>
 
       <Container>
@@ -32,19 +38,33 @@ export function NewTransactionModal({
         <input type="number" placeholder="Valor" />
 
         <TransactionTypeContainer>
-          <button type='button'>
-            <img src={incomeImg} alt='Entrada'/>
+          <RadioBox
+            onClick={() => {
+              setType("deposit");
+            }}
+            type="button"
+            isActive={type === 'deposit'}
+            activeColor="green"
+          >
+            <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
-          </button>
-          <button type='button'>
-            <img src={outcomeImg} alt='Saída'/>
+          </RadioBox>
+          <RadioBox
+            isActive={type === 'withdraw'}
+            activeColor="red"
+            onClick={() => {
+              setType("withdraw");
+            }}
+            type="button"
+          >
+            <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
-          </button>
+          </RadioBox>
         </TransactionTypeContainer>
 
         <input placeholder="Categoria" />
 
-        <button type="submit" >Cadastrar</button>
+        <button type="submit">Cadastrar</button>
       </Container>
     </Modal>
   );
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {darken} from 'polished';
+import {darken, transparentize} from 'polished';
 export const Container = styled.form`
  h2 {
    color: var(--text-title);
@@ -48,17 +48,29 @@ export const TransactionTypeContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
+`;
 
-  button {
+interface RadioBoxProps {
+  isActive: boolean;
+  activeColor: 'green' | 'red';
+}
+
+const colors = {
+  green: '#33CC95',
+  red: '#E52E4D'
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
     height: 4rem;
     border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
-    background: transparent;
+    background: ${(props) => props.isActive 
+    ? transparentize(0.8, colors[props.activeColor])
+    : 'transparent'};
     display: flex;
     align-items: center;
     justify-content: center;
     
-
     &:hover {
       border-color: ${darken(0.1, '#d7d7d7')};
     }
@@ -74,5 +86,4 @@ export const TransactionTypeContainer = styled.div`
       font-size: 1rem;
       color: var(--text-title);
     }
-  }
 `
